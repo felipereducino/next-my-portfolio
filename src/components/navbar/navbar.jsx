b34cbfd6d4795bf7fb6cb31a842617bc729c125a
Felipe Reducino <felipe.reducino@fretebras.com.br>
2024-05-04 14:50:28 -0300

    feat: adds framer-motion library

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,65 +5,24 @@ import Image from "next/image";
 import { useState } from "react";
 import { motion } from "framer-motion";
 import NavLink from "../navLink/navLink";
+import {
+  topVariants,
+  bottomVariants,
+  centerVariants,
+  listItemVariants,
+  listVariants,
+} from "./navbar-motion-variants";
+
 const links = [
   { url: "/", title: "Home" },
   { url: "/about", title: "About" },
   { url: "/portfolio", title: "Portfolio" },
   { url: "/contact", title: "Contact" },
 ];
+
 const Navbar = () => {
   const [open, setOpen] = useState(false);
-  const topVariants = {
-    closed: {
-      rotate: 0,
-    },
-    opened: {
-      rotate: 45,
-      backgroundColor: "rgb(255,255,255)",
-    },
-  };
-  const centerVariants = {
-    closed: {
-      opacity: 1,
-    },
-    opened: {
-      opacity: 0,
-    },
-  };
 
-  const bottomVariants = {
-    closed: {
-      rotate: 0,
-    },
-    opened: {
-      rotate: -45,
-      backgroundColor: "rgb(255,255,255)",
-    },
-  };
-
-  const listVariants = {
-    closed: {
-      x: "100vw",
-    },
-    opened: {
-      x: 0,
-      transition: {
-        when: "beforeChildren",
-        staggerChildren: 0.2,
-      },
-    },
-  };
-
-  const listItemVariants = {
-    closed: {
-      x: -10,
-      opacity: 0,
-    },
-    opened: {
-      x: 0,
-      opacity: 1,
-    },
-  };
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
       {/* LINKS */}
---
+++ src/components/transtitionProvider/transitionProvider.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
+import Navbar from "../navbar/navbar";
+import { usePathname } from "next/navigation";
+
+const TransitionProvider = ({ children }) => {
+  const pathName = usePathname();
+  return (
+    <AnimatePresence mode="wait">
+      <div
+        key={pathName}
+        className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100"
+      >
+        <motion.div
+          className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
+          animate={{ height: "0vh" }}
+          exit={{ height: "140vh" }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+        />
+        <motion.div
+          className="h-screen w-screen fixed bg-black rounded-t-[100px] bottom-0 z-30"
+          initial={{ height: "140vh" }}
+          animate={{ height: "0vh", transition: { delay: 0.5 } }}
+        />
+        <div className="h-24">
+          <Navbar />
+        </div>
+        <div className="h-[calc(100vh-6rem)]">{children}</div>
+      </div>
+    </AnimatePresence>
+  );
+};
+export default TransitionProvider;
